75128b947d378b98b31340c93809ccfef9346a77
Saulo Silva Castillos <altfreedom@gmail.com>
2019-12-27 15:20:13 -0300

    gh-pages repository

---
+++ precache-manifest.d2cfe2af9d2d3d43393515e5dc180337.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d576be51411a7560028a9ff60a95c0b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35e75c97986016bad25f",
+    "url": "/static/js/2.39f90471.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.39f90471.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4f0b85a91e8c1ffbff3",
+    "url": "/static/js/main.701bcc59.chunk.js"
+  },
+  {
+    "revision": "4309999aee2eb2f1d04a",
+    "url": "/static/js/runtime-main.11f7022d.js"
+  }
+]);

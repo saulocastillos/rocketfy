88bc82c3021fa7f1f97462a914d15ef6268942d9
Saulo Silva Castillos <altfreedom@gmail.com>
2019-12-27 15:41:05 -0300

    Updates

---
+++ precache-manifest.b28ffffbaed2b47db6e30f1861dec4bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fab84bf4683a00abb855dfde329a6cd",
+    "url": "/rocketfy/index.html"
+  },
+  {
+    "revision": "aa5b20a2c38c2f81f0e4",
+    "url": "/rocketfy/static/js/2.31b8ff52.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/rocketfy/static/js/2.31b8ff52.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f260fd482d7ad67b0069",
+    "url": "/rocketfy/static/js/main.300e0dac.chunk.js"
+  },
+  {
+    "revision": "cfe779644af81e945766",
+    "url": "/rocketfy/static/js/runtime-main.d1e888eb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.839d574b4204932ad3d81b592622c216.js"
+  "/rocketfy/precache-manifest.b28ffffbaed2b47db6e30f1861dec4bc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rocketfy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
